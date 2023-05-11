32f1049a9799e3640e46ff66cee25cd89ae3b574
Jahi Grant <jahigrant@googlemail.com>
2023-05-11 15:27:36 +0100

    Initial commit

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,11 +1,7 @@
-document.querySelectorAll('.be-logo-bold').forEach((node) => {
-    node.innerHTML= `<img class="mb-2" src="assets/be-logo.svg" width="32" height="32">`
-});
-
-document.querySelectorAll('.be-logo').forEach((node) => {
-    node.innerHTML= `<img class="mb-0" src="assets/be-logo.svg" width="24" height="24">`
-});
-
-document.querySelectorAll('.ma-logo-plain').forEach((node) => {
-    node.innerHTML= `<img class="pb-2" src="assets/malarkes-logo-plain.svg" height="28">`
+//refresh page on browser resize
+$(window).bind('resize', function(e)
+{
+console.log('window resized..');
+this.location.reload(false); /* false to get page from cache */
+/* true to fetch page from server */
 });
