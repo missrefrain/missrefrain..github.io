327fb9435e99aa2be60f16326edb5beab5d4fffd
Jahi Grant <mussygrant@gmail.com>
2023-05-14 01:19:37 +0100

    Update script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-//refresh page on browser resize
-$(window).bind('resize', function(e)
-{
-console.log('window resized..');
-this.location.reload(false); /* false to get page from cache */
-/* true to fetch page from server */
-});
