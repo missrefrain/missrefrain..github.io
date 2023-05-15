f0e907876b6331626578393a0c5ac0c77574cf06
Jahi Grant <jahigrant@googlemail.com>
2023-05-15 02:29:53 +0100

    Initial commit

--- _site/js/script.js
+++ _site/js/script.js
@@ -8,5 +8,20 @@ $(document).ready(function(){
     });
 
 
+    $('#choices-multiple-remove-button').on('change', function() {
+        let services = document.getElementById("choices-multiple-remove-button");
+        let totalinput = document.getElementById("servicesTotal");
+        totalinput.value = ""
+        
+        let y = this.value;
+console.log(y);
+        let z = totalinput.value
+console.log(z);
+        totalinput.value += eval(y+z)
+        
+    });
+
+
+    
 });
    
