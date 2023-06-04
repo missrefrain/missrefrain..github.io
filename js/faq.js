9f4806dc9b44cc739d7ec286782ffd8f162ed918
Jahi Grant <jahigrant@googlemail.com>
2023-06-04 20:58:59 +0100

    Initial commit

---
+++ js/faq.js
@@ -0,0 +1,49 @@
+// $(document).ready(function(){
+
+
+
+//     const get_faq_container = document.getElementById('faq-items');
+//     var faqjson = '{% include mainurl.html %}/_data/faq.json';
+
+
+//     function faq_setup() {
+      
+    
+//     fetch(faqjson)
+//     .then(response => response.json())
+//     .then(responseData => {
+//         responseData.forEach(faqelement => {
+
+// console.log(faqelement);
+
+//         })
+//     })
+//     .catch(error => {
+//         // handle the error
+//     });
+//     }
+
+
+//     // function faq() {
+//     //     const element = (
+    
+//     //         <div>
+//     //             <div id="faq-layout"></div>
+//     //         </div>
+    
+//     //     );
+//     //     ReactDOM.render(
+//     //         element,
+//     //         document.getElementById('faq_component')
+//     //     );
+//     // }
+    
+//     // faq();
+//     faq_setup();
+
+    
+
+// });
+   
+
+
