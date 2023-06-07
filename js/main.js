8bb47e894d53aba8b29caf9b9d089ea851cecafc
Jahi Grant <jahigrant@googlemail.com>
2023-06-07 15:24:55 +0100

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 
 $(document).ready(function(){
-
+    
+        var current = location.pathname;
+        $('#navbarNav ul li a').each(function() {
+            var $this = $(this); 
+    console.log($this);
+            // we check comparison between current page and attribute redirection.
+            
+            if ($this.attr('href') === current) {
+               $this.addClass('active'); 
+            }
+        });
+ 
 
 });
    
